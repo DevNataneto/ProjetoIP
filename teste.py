024797ccf46fe62a917f64728ce5302d655f628c
viniciusolzon <91697213+viniciusolzon@users.noreply.github.com>
2021-11-17 15:49:27 -0400

    testando

---
+++ teste.py
@@ -0,0 +1,8 @@
+a= float(input('digite um numero:'))
+b= float(input('digite outro numero qualquer:'))
+if a > b:
+    print("o primeiro é maior que o segundo!")
+elif a < b:
+    print("o primeiro é menor que o segundo!")
+elif a == b:
+    print("eles são iguais!")
