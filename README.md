d78164320284df394dae8822420091b9f2528b62
Vinicius <vinicius_freitas03@hotmail.com>
2021-11-17 16:06:08 -0400

    mudei dnv

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ProjetoIP
 Projeto da disciplina de Introdução a Programação do Curso de Ciência de Dados e Inteligência Artificial da UFPB
+print ('hello world')
--- teste.py
+++ teste.py
@@ -1,8 +1,2 @@
-a= float(input('digite um numero:'))
-b= float(input('digite outro numero qualquer:'))
-if a > b:
-    print("o primeiro é maior que o segundo!")
-elif a < b:
-    print("o primeiro é menor que o segundo!")
-elif a == b:
-    print("eles são iguais!")
+xyz=1000
+print(xyz)
