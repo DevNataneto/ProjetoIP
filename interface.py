09dc3dd12e53a826473e586cbfa7d0fb231988f3
viniciusolzon <91697213+viniciusolzon@users.noreply.github.com>
2021-11-20 12:24:07 -0400

    interface

--- interface.py
+++ interface.py
@@ -1,3 +1,30 @@
-frase= input('digite a sua frase:')
-for letra in frase:
-    print (letra)
+import pygame
+from pygame.time import Clock
+
+#elementos_da_tela
+tela_altura = 600
+tela_largura = 1200 
+tela = pygame.display.set_mode((tela_largura,tela_altura))
+fps = pygame.time.Clock()
+pygame.display.set_caption('Space Invaders')
+
+#inicio_jogo
+pygame.init
+#jogo_rodando
+while True:
+    #pega_eventos_do_jogo
+    for evento in pygame.event.get():
+    #Quit_game  
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    #elementos_da_tela
+    tela.fill(pygame.Color('darkblue'))
+
+
+    #atualiza_tela
+    pygame.display.update()
+    fps =  60
+    
+pygame.quit()
