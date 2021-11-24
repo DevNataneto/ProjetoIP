920dee3ddd1a24a2dba17070e2663d22b42d5537
Vinicius <vinicius_freitas03@hotmail.com>
2021-11-24 13:20:03 -0400

    jogador

--- jogador.py
+++ jogador.py
@@ -1,5 +1,10 @@
 import pygame 
+
 #codigos_jogador:
-
-
-
+class Jogador(pygame.sprite.Sprite):
+    def __init__(self, x , y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("img\jogador.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+    
