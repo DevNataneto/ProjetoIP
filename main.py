604238fe7907f177820b8aa2701f2d269b4e1c4e
Vinicius <vinicius_freitas03@hotmail.com>
2021-11-24 20:19:36 -0400

    jogador_e_vida

--- main.py
+++ main.py
@@ -4,17 +4,15 @@ from pygame.time import Clock
 #codigos_jogador
 class Jogador(pygame.sprite.Sprite):
     #cria_jogador
-    def __init__(self, x , y, vida):
+    def __init__(self, x , y):
         pygame.sprite.Sprite.__init__(self)
         self.image_small = pygame.image.load("imagens\jogador_nave.png")
         self.image = pygame.transform.scale(self.image_small, (65,65))
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
-        self.vida = vida
-        self.dano = vida
 
     #atualiza_tudo
-    def atualiza(self):
+    def movimento(self):
         velocidade = 1
         aperta = pygame.key.get_pressed()
         #movimentos_jogador
@@ -26,21 +24,61 @@ class Jogador(pygame.sprite.Sprite):
             self.rect.y -= velocidade
         if aperta[pygame.K_DOWN]:
             self.rect.y += velocidade
-
         #limita_jogador    
         if self.rect.top <= 0:
            self.rect.top = 0
-        if self.rect.bottom >= (tela_altura - 30):
-            self.rect.bottom = (tela_altura - 30)
+        if self.rect.bottom >= (tela_altura - 35):
+            self.rect.bottom = (tela_altura - 35)
+        if self.rect.left <= 30:
+            self.rect.left = 30
+        if self.rect.right >= tela_largura - 30:
+            self.rect.right = tela_largura - 30
+
+#vida_jogador
+class Vidinhas(pygame.sprite.Sprite):
+    def __init__(self, x , y, vida):
+        pygame.sprite.Sprite.__init__(self)
+        if vida == 3:
+            self.image_small = pygame.image.load("imagens\coracoes_3.png")
+        if vida == 2:
+            self.image_small = pygame.image.load("imagens\coracoes_2.png")
+        if vida == 1:
+            self.image_small = pygame.image.load("imagens\coracoes_1.png")
+        if vida == 0:
+            self.image_small = pygame.image.load("imagens\coracoes_0.png")
+        self.image = pygame.transform.scale(self.image_small, (140,35))
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+    def movimento(self):
+        velocidade = 1
+        aperta = pygame.key.get_pressed()
+        #movimentos_vidas
+        if aperta[pygame.K_RIGHT]:
+            self.rect.x += velocidade
+        if aperta[pygame.K_LEFT]:
+            self.rect.x -= velocidade
+        if aperta[pygame.K_UP]:
+            self.rect.y -= velocidade
+        if aperta[pygame.K_DOWN]:
+            self.rect.y += velocidade
+        #limita_vida  
+        if self.rect.top <= 65:
+           self.rect.top = 65
+        if self.rect.bottom >= (tela_altura - 5):
+            self.rect.bottom = (tela_altura - 5)
         if self.rect.left <= 0:
             self.rect.left = 0
-        if self.rect.right >= tela_largura:
-            self.rect.right = tela_largura
+        if self.rect.right >= tela_largura + 15:
+            self.rect.right = tela_largura + 15
 
-        #vida_jogador
-        pygame.draw.rect(tela, vermelho, (self.rect.x, (self.rect.bottom + 10), self.rect.width, 15))
-        if self.dano > 0:
-            pygame.draw.rect(tela, verde, (self.rect.x, (self.rect.bottom + 10), int(self.rect.width * (self.dano / self.vida)), 15))
+class Tiros(pygame.sprite.Sprite):
+    #cria_tiros
+    def __init__(self, x , y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image_small = pygame.image.load("imagens\coracoes_0.png")
+        self.image = pygame.transform.scale(self.image_small, (65,65))
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
 
 #elementos_base_da_tela
 tela_largura = 1200
@@ -50,16 +88,15 @@ fps = pygame.time.Clock()
 pygame.display.set_caption('Space Invaders')
 fundo_small = pygame.image.load("imagens\espaço.png")
 fundo = pygame.transform.scale(fundo_small, (tela_largura,tela_altura))
-#cores
-vermelho = (255,0,0)
-verde = (0,255,0)
 
 #todos_sprites
 grupo_jogador= pygame.sprite.Group()
 
 #elementos_add_da_tela
-jogador = Jogador(int(tela_largura / 2), tela_altura - 100, 3)
+jogador = Jogador(int(tela_largura / 2), tela_altura - 100)
+vidas = Vidinhas(int(tela_largura / 2 + 6), tela_altura - 50, 3)
 grupo_jogador.add(jogador)
+grupo_jogador.add(vidas)
 
 
 #inicio_jogo
@@ -74,8 +111,8 @@ while True:
             sys.exit()
     tela.blit(fundo, (0,0))
     #atualiza_jogador
-    jogador.atualiza()
-    
+    jogador.movimento()
+    vidas.movimento()
     #elementos_na_tela
     grupo_jogador.draw(tela)
 
